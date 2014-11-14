daa6990e1021b14f33439f96112c6c1c7cad6f4c
Spencer Cheng <spenccheng@gmail.com>
2014-11-14 12:52:08 +0000

    final changes1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+McdonaldsWeek2014::Application.config.secret_key_base = '6572cad85bf78787c088e1a80560e4fe868ce4600613787af687590c0b75804757b117db348cb3e2368ff7d8400b1e21a91df9d025ba730fc7dd74d92ab40598'
